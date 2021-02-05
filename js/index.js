2e50a50da93db7ba854a3c0a51e63520783159f8
AndreaJasper <andrea.nicole.jasper@gmail.com>
2021-02-05 13:36:50 -0700

    Done.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
 const app = "I don't do much.";
+
+const toke = 'dfed2f1224cdf062d7ed6ff68e3d82480989e41d'
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+})
+  .then(res => res.json())
+  .then(json => console.log(json));
